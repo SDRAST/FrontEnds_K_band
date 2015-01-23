4bebdcb3a7b46f6cdc9d5532cac13026c0d8b4c3
Tom Kuiper <kuiper@JPL.NASA.GOV>
2015-01-23 22:19:23 +0000

    Working on WBDC2 M&C

--- __init__.py
+++ __init__.py
@@ -9,6 +9,8 @@ import copy
 from MonitorControl import Port, Beam, ComplexSignal, ObservatoryError
 from MonitorControl.FrontEnds import FrontEnd
 
+module_logger = logging.getLogger(__name__)
+
 class K_4ch(FrontEnd):
   """
   The 4-channel downconverter with four inputs for two pols and two feeds.
@@ -33,24 +35,34 @@ class K_4ch(FrontEnd):
     @type  active : bool
     """
     if inputs == None:
-      inputs = K_4ch.feeds
+      # This is for Receiver stand-alone testing
+      inputs = {}
+      for feed in K_4ch.feeds:
+        inputs[feed] = Port(self, feed, signal=Beam(feed))
+    self.name = name
+    mylogger = logging.getLogger(module_logger.name+".K_4ch")
+    mylogger.debug(" initializing %s", self)
+    mylogger.info(" %s input channels: %s", self, str(inputs))
+    mylogger.debug(" output names: %s", output_names)
     FrontEnd.__init__(self, name, inputs=inputs, output_names=output_names)
-    self.logger.debug(" initializing K_4ch subclass %s", self)
-    self.logger.info(" %s input channels: %s", self, str(self.inputs))
+    self.logger = mylogger
     self.channel = {}
+    # These are receiver properties as well as signal properties
     self.data['frequency'] = 22.0 # GHz
-    self.data['bandwidth'] = 10. # GHz
+    self.data['bandwidth'] = 10.  # GHz
     keys = self.inputs.keys()
     keys.sort()
     for feed in keys:
       index = keys.index(feed)
-      beam_signal = copy.copy(self.inputs[feed].signal)
-      beam_signal.name = name+feed
+      beam_signal = Beam(feed)
+      for prop in self.data.keys():
+        beam_signal.data[prop] = self.data[prop]
+      #beam_signal.name = feed
       self.channel[feed] = self.Channel(self, feed,
                                         inputs={feed: self.inputs[feed]},
                                         output_names=output_names[index],
                                         signal=beam_signal)
-    self.logger.info(" %s output channel: %s", self, str(self.outputs))
+    self.logger.info("%s output channels: %s", self, str(self.outputs))
     self.set_ND_off()
 
   def _update_self(self):
@@ -64,22 +76,24 @@ class K_4ch(FrontEnd):
                  signal=None, active=True):
       """
       """
-      self.logger = logging.getLogger(parent.logger.name+".Channel")
+      mylogger = logging.getLogger(parent.logger.name+".Channel")
+      self.name = name
+      mylogger.debug(" initializing for %s", self)
       FrontEnd.Channel.__init__(self, parent, name, inputs=inputs,
                                   output_names=output_names, active=active)
-      self.logger.debug(" initializing K_4ch channel %s", self)
+      self.logger = mylogger
       self.logger.debug(" %s inputs: %s", self, str(inputs))
       for pol in K_4ch.pols:
         index = K_4ch.pols.index(pol)
         ID = output_names[index]
         self.outputs[ID] = Port(self, ID,
                                 source=inputs[name],
-                                signal=ComplexSignal(signal, pol))
+                                signal=ComplexSignal(signal, name=pol, pol=pol))
         #self.outputs[ID].signal.FITS['OBSFREQ'] = parent['frequency']*1e9 # Hz
         #self.outputs[ID].signal.FITS['BANDWID'] = parent['bandwidth']*1e9 # Hz
-        self.outputs[ID].signal['beam'] = parent.name+name
-        self.outputs[ID].signal['frequency'] = parent['frequency']*1e9 # Hz
-        self.outputs[ID].signal['bandwidth'] = parent['bandwidth']*1e9 # Hz
+        #self.outputs[ID].signal['beam'] = parent.name+name
+        #self.outputs[ID].signal['frequency'] = parent['frequency']*1e9 # Hz
+        #self.outputs[ID].signal['bandwidth'] = parent['bandwidth']*1e9 # Hz
         parent.outputs[ID] = self.outputs[ID]
       self.logger.debug(" %s outputs: %s", self, str(self.outputs))
       self.retract_load()
